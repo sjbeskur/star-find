2b4599704dddd49ab6743d3dcab61190291f7dfa
sjbeskur <sam.beskur@gmail.com>
2023-11-05 01:07:08 -0600

    stub in code for collecting components magnitude

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 #![allow(dead_code)]
 mod cli;
-use log::{debug}; //info, trace, warn, error, 
+use log::{debug}; use std::collections::HashMap;
+//info, trace, warn, error, 
 use std::error::Error;
 use std::fs::File;
 use std::io::{ BufRead, BufReader };
+use std::ops::Deref;
 use serde::{ Serialize, Deserialize };
 
 use opencv::{
     prelude::*,
+    core::Scalar,
     core,
     imgcodecs,
     imgproc,
@@ -55,14 +58,17 @@ pub fn run(config: Config) -> AppResult<Vec<BlobStats>> {
 fn find_stars(src: Mat, connectivity: i32) -> AppResult<Vec<BlobStats>>{
 
     // Threshold it so it becomes binary
-    let mut thresh = Mat::default();
-    imgproc::threshold(&src, &mut thresh, 0.0, 255.0, imgproc::THRESH_BINARY | imgproc::THRESH_OTSU)?;
+    //let mut thresh = Mat::default();
+    //imgproc::threshold(&src, &mut thresh, 0.0, 65535.0, imgproc::THRESH_OTSU)?;
     
     // Perform the operation
     let mut labels = Mat::default();
     let mut stats = Mat::default();
     let mut centroids = Mat::default();
-    let output = imgproc::connected_components_with_stats(&thresh, &mut labels, &mut stats, &mut centroids, connectivity, core::CV_16U);//core::CV_32S);
+    let output = imgproc::connected_components_with_stats(&src, &mut labels, &mut stats, &mut centroids, connectivity, core::CV_32S);// core::CV_16U);//core::CV_32S);
+
+
+    let label_map = map_labels_to_pixel_magnitudes(&labels, &src)?;
 
     let mut blobs = Vec::new();
     for r in 1..stats.rows(){    // 0 is the background
@@ -74,6 +80,8 @@ fn find_stars(src: Mat, connectivity: i32) -> AppResult<Vec<BlobStats>>{
             width:  *stats.at_2d::<i32>(r, imgproc::CC_STAT_WIDTH)? ,
             height: *stats.at_2d::<i32>(r, imgproc::CC_STAT_HEIGHT)? ,
             area:   *stats.at_2d::<i32>(r, imgproc::CC_STAT_AREA)? ,
+            // todo:
+            // magnitude:  label_map.get(  labels.at_2d(y, x)) // something like this
         };
         blobs.push(stat);
     }
@@ -85,6 +93,27 @@ fn find_stars(src: Mat, connectivity: i32) -> AppResult<Vec<BlobStats>>{
 
     Ok(blobs)
 
+}
+
+
+/// Raster scan the label image (Mat) pixel by pixel
+/// to find non zero labels.  If a label is found get the corresponding
+/// pixel from ths src image and accumulate these in the label_map.
+/// NOTE:  this is currently not totally correct because as I'm not handling
+///        multi-byte pixels.
+fn map_labels_to_pixel_magnitudes(labels: &Mat, src: &Mat) -> Result<HashMap<i32,f32>, Box<dyn Error>> {
+    let mut label_map: HashMap<i32,f32> = HashMap::new();
+    for y in 0..(labels.rows()) {
+        for x in 0..(labels.cols()){
+            let label = *labels.at_2d::<i32>(y,x)?;
+            if label != 0{
+                let value = src.at_2d::<u8>(y,x)?;
+                let vv = f32::from(*value);
+                label_map.entry(label).and_modify(|v| { *v += vv} ).or_insert(vv);
+            }
+        }
+    }
+    Ok(label_map)
 }
 
 
